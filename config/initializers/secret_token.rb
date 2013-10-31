e807934773e5b01ac0a5b0bc8a8bda16a7a4d6c9
Nate Andorsky <nandorsky@gmail.com>
2013-10-31 12:10:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Csl::Application.config.secret_token = 'd4c2d62fbc433b688f5ea6e034bd69920016238ba94874ce02dd26146c2679da149694e0d0562d92e823e4c33022f91f82f038ab7eb234b81f0e2a24ffb35938'
